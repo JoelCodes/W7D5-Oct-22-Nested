b17774b33cbdecd099cb082de5db8564385f0b4e
JoelCodes <amnshinness@yahoo.com>
2018-12-07 10:13:42 -0800

    Did a bunch of stuff!! Did not commit incrementally!! Lectures are crazy!!

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # resources :articles do
+  #   resources :comments, only: [:index, :create, :new]
+  # end
+  # resources :comments, only: [:show, :update, :edit, :destroy]
+  resources :articles do
+    resources :comments, shallow: true
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
